533090bcd86a33cc7e92882c141bbe35512d12b6
rholibobo <rholibobo@gmail.com>
2025-03-19 16:12:01 +0100

    updates

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -84,7 +84,7 @@ export default function LoginForm() {
   }
 
   return (
-    <Paper elevation={3} sx={{ p: 3, width: { xs: "90%", md: "50%" } }}>
+    <Paper elevation={3} sx={{ p: 3, width: { xs: "90%", md: "40%" } }}>
       <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
         <Controller
           name="email"
